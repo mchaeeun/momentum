8374696f4b006061691d41192cc04209ffbf37ed
mchaeeun <codmslove281@gmail.com>
2023-08-21 23:18:46 +0900

    ADD real time

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hi");
+const realTime = document.querySelector('#real-time');
+
+function updateTime() {
+  const now = new Date();
+  const nowTime = now.toLocaleTimeString();
+  realTime.textContent = nowTime;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
