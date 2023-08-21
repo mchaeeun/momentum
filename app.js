bc147d17f2ed3e5dbc21625ecb9cec4e9f3ab6e1
mchaeeun <codmslove281@gmail.com>
2023-08-21 23:53:29 +0900

    ADD random background image

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// random background image
+
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+function randomImage() {
+    const chosenImage = images[Math.floor(Math.random() * images.length)];
+    bgImageSrc = `src/img/${chosenImage}`;
+    document.body.style.backgroundImage = `url(${bgImageSrc})`;
+}
+
+randomImage();
+
+// real time clock
+
 const realTime = document.querySelector('#real-time');
 
 function updateTime() {
